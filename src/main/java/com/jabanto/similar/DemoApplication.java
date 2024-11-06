493552a3d6451fc750c729876fa6b8c0a58bedf0
Juan Manuel Abanto <jmanuelabanto@gmail.com>
2024-11-06 14:28:46 -0500

    load project from spring initializr

---
+++ src/main/java/com/jabanto/similar/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.jabanto.similar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
